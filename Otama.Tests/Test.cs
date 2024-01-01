60be45c28cffcea628fad80555531764db3ce67b
usagiga <usagiga@usagiga.me>
2024-01-01 22:25:17 +0900

    test: add example test to examine xUnit behavior and set it up correctly

---
+++ Otama.Tests/Test.cs
@@ -0,0 +1,13 @@
+namespace Otama;
+
+public class Test
+{
+    [Fact (DisplayName = "To test running xUnit correctly")]
+    public void ExampleTest()
+    {
+        var expected = "Hello, World";
+        var actual = "Hello, World";
+
+        Assert.Equal(expected, actual);
+    }
+}
