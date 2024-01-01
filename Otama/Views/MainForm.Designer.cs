c0602bc13d695980e2d1bfd47eec43906d182368
usagiga <usagiga@usagiga.me>
2024-01-02 03:06:37 +0900

    chore: fix co-location

--- Otama/Views/MainForm.Designer.cs
+++ Otama/Views/MainForm.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Otama;
+﻿namespace Otama.Views;
 
-partial class Form1
+partial class MainForm
 {
     /// <summary>
     ///  Required designer variable.
---
+++ Otama/Views/MainForm.cs
@@ -0,0 +1,9 @@
+namespace Otama.Views;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+}
